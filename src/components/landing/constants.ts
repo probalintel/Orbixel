d99eca7b3ec987d9ab9d3533f9588f55a83599f7
Probal Saha <contact.probalsaha@gmail.com>
2026-04-04 20:40:36 +0530

    Delete src/components/landing directory

--- src/components/landing/constants.ts
+++
@@ -1,5 +0,0 @@
-export const WHATSAPP_NUMBER = "918910040655";
-export const WHATSAPP_URL = `https://wa.me/${WHATSAPP_NUMBER}?text=Hi%20Orbixel!%20I'm%20interested%20in%20getting%20a%20website.`;
-export const EMAIL = "Orbixel@gmail.com";
-export const PHONE1 = "8910040655";
-export const PHONE2 = "6291621550";
